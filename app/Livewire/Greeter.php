4f396d28b276f5c93efb577c5e9806b027f08494
thbappy7706 <tanvir15-7706@diu.edu.bd>
2025-12-31 22:42:15 +0600

    validate

--- app/Livewire/Greeter.php
+++ app/Livewire/Greeter.php
@@ -2,19 +2,36 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use function Symfony\Component\Translation\t;
 
 class Greeter extends Component
 {
 
-    public $name = 'John';
+    #[Validate('required|min:2')]
+    public $name = '';
+    public $greeting = '';
+
+    public $greetingMessage = '';
+
+//    public function rules()
+//    {
+//        return [
+//            'name' => 'required|min:2',
+//        ];
+//    }
+
+
     public function render()
     {
         return view('livewire.greeter');
     }
 
-    public function changeName($name): void
+    public function changeGreeting()
     {
-        $this->name = $name;
+        $this->reset('greetingMessage');
+        $this->validate();
+        $this->greetingMessage = "{$this->greeting}, {$this->name}";
     }
 }
--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,10 +1,17 @@
 <div>
-    <div>
-        Hello {{ $name }}!
-    </div>
+    <form wire:submit="changeGreeting()">
+        <select class="border rounded p-4" wire:model.fill="greeting">
+            <option value="Hello">Hello</option>
+            <option value="Hi">Hi</option>
+            <option value="Howdy" selected>Howdy</option>
+            <option value="Hola">Hola</option>
 
-    <form wire:submit="changeName(document.querySelector('#name').value)">
-        <input type="text" id="name" placeholder="Enter your name" class="border rounded px-2 py-1">
+        </select>
+
+        <input type="text"  placeholder="Enter your name" class="border rounded px-2 py-1 mt-3" wire:model="name" />
+        <br/>
+
+        @error('name') <span class="text-red-500">{{ $message }}</span> @enderror
 
 
         <div class="mt-4">
@@ -13,5 +20,13 @@
             </button>
         </div>
     </form>
+
+    @if($greetingMessage !== '')
+        <div>
+            <h1 class="mt-4 text-2xl font-bold">{{ $greetingMessage }} !</h1>
+
+        </div>
+
+    @endif
 
 </div>
